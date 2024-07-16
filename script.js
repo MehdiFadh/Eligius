ef77cc9842b5b3520e5f66247ed21dd495b90b07
MehdiFadh <mfadhloune@gmail.com>
2024-07-17 01:26:03 +0200

    Page de Connexion terminer reste la page home qui suit

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
 document.getElementById('themeToggle').addEventListener('click', function() {
     document.body.classList.toggle('light-theme');
 });
+
+
+document.getElementById('loginForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+    const errorMessage = document.getElementById('errorMessage');
+    
+    const user = users.find(u => u.username === username && u.password === password);
+    
+    if (user) {
+        errorMessage.textContent = '';
+        alert('Connexion réussie !');
+        window.location.href = 'home.html';  // Redirection vers la page d'accueil après connexion
+    } else {
+        errorMessage.textContent = 'Nom d\'utilisateur ou mot de passe incorrect.';
+    }
+});
