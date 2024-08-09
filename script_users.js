6bf3b4882736eae30837fb37e5cecb85a5c25466
MehdiFadh <mfadhloune@gmail.com>
2024-08-09 16:19:30 +0100

    Ajout d'une page interdiction pour une connexion interdite

--- script_users.js
+++ script_users.js
@@ -5,8 +5,12 @@ document.getElementById('butTheme').addEventListener('click', function () {
 document.addEventListener('DOMContentLoaded', function () {
     // Vérifiez l'authentification sur les pages protégées
     if (window.location.pathname.endsWith('home_user1.html') && localStorage.getItem('isAuthenticated') !== 'true' || window.location.pathname.endsWith('home_user2.html') && localStorage.getItem('isAuthenticated') !== 'true' || window.location.pathname.endsWith('home_user3.html') && localStorage.getItem('isAuthenticated') !== 'true' ) {
-    window.location.href = 'index.html';  // Redirection vers la page de connexion si non authentifié
+    window.location.href = 'interdiction.html';  // Redirection vers la page de connexion si non authentifié
 }
+
+document.getElementById('butQuitter').addEventListener('click', function(){
+    window.location.href = 'index.html';
+})
 
 // Ajout d'un écouteur d'événement pour le bouton de déconnexion
 document.getElementById('butDeconnection').addEventListener('click', function () {
